77d91da0702681f7ad395ed74006fa1fd7850311
AlexSamylovskikh <alex.kmoxville@gmail.com>
2021-08-25 23:58:58 +0600

    Initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fizzBuzzQuery = Enumerable.Range(1, 100).Select(
+                element => {
+                    string result;
+
+                    if (element % 3 == 0 && element % 5 == 0) 
+                    {
+                        result = "fizz buzz";
+                    }
+                    else if (element % 3 == 0)
+                    {
+                        result = "fizz";
+                    }
+                    else if (element % 5 == 0)
+                    {
+                        result = "buzz";
+                    }
+                    else
+                    {
+                        result = element.ToString();
+                    }
+
+                    return result;
+                });
+
+            foreach (string str in fizzBuzzQuery)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
