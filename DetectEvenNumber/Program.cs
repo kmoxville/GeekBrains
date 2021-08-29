c6abe4f89c3a15abf0516ce38502aa28b10b5f80
AlexSamylovskikh <alex.kmoxville@gmail.com>
2021-08-29 20:56:26 +0600

    Урок 2, задача 3

---
+++ DetectEvenNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DetectEvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+
+            int number          = int.Parse(Console.ReadLine());
+            string numberType   = (number % 2 == 0 ? "четное" : "нечетное");
+
+            Console.WriteLine($"Число {number} {numberType}") ;
+        }
+    }
+}
