64b552d6f0ab5a807731d3c7662f0dec5db278ec
AlexSamylovskikh <alex.kmoxville@gmail.com>
2021-09-01 00:24:18 +0600

    Урок 3, задача 1

---
+++ LessonArrays1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace LessonArrays1
+{
+    class Program
+    {
+        private const int ARRAY_LENGTH = 11;
+
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            int[][] array2d = new int[ARRAY_LENGTH][];
+
+            for (int i = 0; i < array2d.Length; i++)
+            {
+                array2d[i] = new int[ARRAY_LENGTH];
+                for (int j = 0; j < array2d[i].Length; j++)
+                {
+                    array2d[i][j] = rand.Next(0, 1000);
+                }
+            }
+
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < array2d.Length; i++)
+            {
+                for (int j = 0; j < array2d[i].Length; j++)
+                {
+                    if ((i == j) || (i+j == array2d.Length-1))
+                        result.Append($"{array2d[i][j], 8}");
+                    else
+                        result.Append("".PadLeft(8));
+                }
+
+                result.AppendLine();
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
