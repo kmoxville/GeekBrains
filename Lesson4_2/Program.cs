dc8d4f0cb655c22b3348fae83abcf91dd86f2802
AlexSamylovskikh <alex.kmoxville@gmail.com>
2021-09-06 23:39:37 +0600

    Урок 4, задание 2

---
+++ Lesson4_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку чисел");
+            
+            string userInput = Console.ReadLine();
+            var userInputParsedQuery = userInput.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(element => 
+                { 
+                    bool parseSuccesfull = decimal.TryParse(element, out var parseResult); 
+                    return (parseSuccesfull, parseResult, element); 
+                });
+
+            decimal sum = userInputParsedQuery.Where(element => element.Item1)
+                .Select(element => element.Item2)
+                .Sum();
+
+            var unparsedInputQuery = userInputParsedQuery.Where(element => !element.Item1)
+                .Select(element => element.Item3);
+
+            Console.WriteLine("Нераспозанный ввод: {0}", string.Join(' ', unparsedInputQuery));
+            Console.WriteLine($"Сумма: {sum}");
+        }
+    }
+}
