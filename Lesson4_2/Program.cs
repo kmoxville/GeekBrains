92e6f4a491a3abf6fcc8de749aaa5c060d19db12
AlexSamylovskikh <alex.kmoxville@gmail.com>
2021-09-07 01:46:58 +0600

    Fixing tuples

--- Lesson4_2/Program.cs
+++ Lesson4_2/Program.cs
@@ -14,15 +14,15 @@ namespace Lesson4_2
                 .Select(element => 
                 { 
                     bool parseSuccesfull = decimal.TryParse(element, out var parseResult); 
-                    return (parseSuccesfull, parseResult, element); 
+                    return (parseSuccesfull: parseSuccesfull, ConvertedValue: parseResult, InitialValue: element); 
                 });
 
-            decimal sum = userInputParsedQuery.Where(element => element.Item1)
-                .Select(element => element.Item2)
+            decimal sum = userInputParsedQuery.Where(element => element.parseSuccesfull)
+                .Select(element => element.ConvertedValue)
                 .Sum();
 
-            var unparsedInputQuery = userInputParsedQuery.Where(element => !element.Item1)
-                .Select(element => element.Item3);
+            var unparsedInputQuery = userInputParsedQuery.Where(element => !element.parseSuccesfull)
+                .Select(element => element.InitialValue);
 
             Console.WriteLine("Нераспозанный ввод: {0}", string.Join(' ', unparsedInputQuery));
             Console.WriteLine($"Сумма: {sum}");
