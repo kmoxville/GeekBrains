ed32ec83b6768328104f86162b43d4aaa7e917f2
AlexSamylovskikh <alex.kmoxville@gmail.com>
2021-09-06 23:05:30 +0600

    Урок 4, задание 1

---
+++ GetFullName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GetFullName
+{
+    class Program
+    {
+        static string GetFullName(string firstName, string lastName, string patronymic = "")
+        {
+            return string.Join(' ', lastName, firstName, patronymic);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetFullName("Иванов", "Иван", "Иванович"));
+            Console.WriteLine(GetFullName("Петров", "Петр", "Петрович"));
+            Console.WriteLine(GetFullName("Васильев", "Василий", "Васильевич"));
+        }
+    }
+}
