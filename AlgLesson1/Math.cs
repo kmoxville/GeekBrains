41b609c8271aab6f43539cc5dc900ae01e4d3938
AlexSamylovskikh <alex.kmoxville@gmail.com>
2021-10-02 21:45:05 +0600

    Урок 1. Задание 1 и 3

---
+++ AlgLesson1/Math.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgLesson1
+{
+    static class Math
+    {
+        /// <summary>
+        /// Задание 1
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static bool IsNumberPrime(int number)
+        {
+            int d = 0, i = 2;
+
+            while (i < number)
+            {
+                if (number % i == 0)
+                    d++;
+                
+                i++;
+            }
+
+            if (d == 0)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Задание 3. Ряд Фибоначчи с рекурсией
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static int FiboRecursive(int number)
+        {
+            if (number <= 1)
+                return number;
+
+            return FiboRecursive(number - 1) + FiboRecursive(number - 2);
+        }
+
+        /// <summary>
+        /// Задание 3. ряд Фибоначчи без рекурсии
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static int FiboCycle(int number)
+        {
+            if (number <= 1)
+                return number;
+
+            int result = 0, prevResult = 1, tmp = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                tmp = result;
+                result = prevResult;
+                prevResult += tmp;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AlgLesson1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AlgLesson1
+{
+    class Program
+    {
+        static int[] primeNumbersTestArray = { 1, 17, 47, 117, 163, 164 };
+        static int[] fiboNumbersTestArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20 };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime test");
+            foreach (int i in primeNumbersTestArray)
+            {
+                string description = Math.IsNumberPrime(i) ? "prime" : "not prime";
+                Console.WriteLine($"{i} is {description}");
+            }
+
+            Console.WriteLine("\nFibonacci recursive test");
+            foreach (int i in fiboNumbersTestArray)
+            {
+                Console.WriteLine($"F({i}) = {Math.FiboRecursive(i)}");
+            }
+
+            Console.WriteLine("\nFibonacci cycle test");
+            foreach (int i in fiboNumbersTestArray)
+            {
+                Console.WriteLine($"F({i}) = {Math.FiboCycle(i)}");
+            }
+        }
+    }
+}
