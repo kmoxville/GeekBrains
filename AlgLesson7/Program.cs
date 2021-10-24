f3d207590e700356cc9eb48acd8fe5ab1cde0a93
AlexSamylovskikh <alex.kmoxville@gmail.com>
2021-10-24 20:48:09 +0600

    урок 7

---
+++ AlgLesson7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AlgLesson7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите N");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите M");
+            int m = int.Parse(Console.ReadLine());
+
+            int[,] field = new int[n - 1, m - 1];
+
+            Console.WriteLine($"Количество маршрутов = {W(n - 1, m - 1)}");
+        }
+
+        private static int W(int n, int m)
+        {
+            if ((n == 0) || (m == 0))
+                return 1;
+
+            return W(n - 1, m) + W(n, m - 1);
+        }
+    }
+}
